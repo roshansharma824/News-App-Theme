717382256ba72ec35efddba3fd39df85c3fd6e41
Roshan Kumar Sharma <roshan@conscent.ai>
2024-12-17 15:20:43 +0530

    remove comment

--- app/src/main/java/com/example/newsapptheme/presentation/component/NewsCard.kt
+++ app/src/main/java/com/example/newsapptheme/presentation/component/NewsCard.kt
@@ -51,7 +51,7 @@ fun NewsCard(item: News, index: Int, onItemClicked: (Int) -> Unit) {
             .fillMaxWidth()
             .wrapContentHeight(),
         onClick = {
-//            openUrl(item.url)
+
             onItemClicked.invoke(index)
         }
     ) {
